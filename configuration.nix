9957db937c6879b0fbef26236a3cc4c0a0c5c5c8
Will Sturgeon <willstrgn@gmail.com>
2025-11-01 01:00:52 -0700

    Automatic build succeeded

--- configuration.nix
+++ configuration.nix
@@ -915,8 +915,7 @@ in
           src = inputs.google-fonts;
           dontBuild = true;
           installPhase = ''
-            mkdir -p $out/share/fonts
-            # find . -name '*.ttf' -exec install -m 444 -Dt $out/share/fonts/truetype '{}' +
+            find . -name '*.ttf' -exec install -m 444 -Dt $out/share/fonts/truetype '{}' +
             find . -name '*.otf' -exec install -m 444 -Dt $out/share/fonts/opentype '{}' +
           '';
         }
