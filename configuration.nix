1ade884c837d94b2d569a3f786d7e7d898a5f21e
Will Sturgeon <willstrgn@gmail.com>
2025-11-05 17:07:02 -0800

    Remove `networking` for non-NixOS

--- configuration.nix
+++ configuration.nix
@@ -123,11 +123,6 @@ in
     ;
   };
 
-  networking.hostName = hostname; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
-
   # Set your time zone.
   time.timeZone = "America/Los_Angeles";
 
@@ -1009,6 +1004,11 @@ in
       #   keyMap = "us";
       #   useXkbConfig = true; # use xkb.options in tty.
       # };
+
+      networking.hostName = hostname; # Define your hostname.
+      # Pick only one of the below networking options.
+      # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+      networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
 
     }
   else
