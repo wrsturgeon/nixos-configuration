2fd888e10d2f30168422a2ed467608d3878881a3
Will Sturgeon <willstrgn@gmail.com>
2026-04-18 13:44:19 -0700

    Automatic build succeeded

--- configuration.nix
+++ configuration.nix
@@ -629,25 +629,8 @@ in
         tapping = false;
       };
     };
-    # llama-cpp = {
-    #   host = llama-cpp-host;
-    #   modelsPreset = {
-    #     "bonsai" = {
-    #       c = 32768; # 0;
-    #       hf-file = "Bonsai-8B.gguf";
-    #       hf-repo = "prism-ml/bonsai-8b-gguf";
-    #       min-p = 0;
-    #       ngl = 99;
-    #       temp = 0.5;
-    #       top-k = 20;
-    #       top-p = 0.85;
-    #     };
-    #   };
-    #   package = llama-cpp;
-    #   port = llama-cpp-port;
-    # };
     logind.settings.Login = {
-      HandleLidSwitch = "ignore";
+      # HandleLidSwitch = "ignore";
       HandleLidSwitchExternalPower = "ignore";
       HandleLidSwitchDocked = "ignore";
     };
