764447ec00f4b8019b6b299d61336e38c3101af3
Will Sturgeon <willstrgn@gmail.com>
2026-05-03 12:06:05 -0700

    Automatic build succeeded

--- configuration.nix
+++ configuration.nix
@@ -390,10 +390,7 @@ in
         };
         nix-index = { };
         nixvim = {
-          colorschemes = {
-            # ayu.enable = true;
-            everforest.enable = true;
-          };
+          colorschemes.ayu.enable = true;
           dependencies.lean.enable = lib.mkForce false;
           diagnostic.settings.virtual_text = true;
           opts = rec {
