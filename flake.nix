3ba8aa22b4bb2cfe07a7c986577cb8ccb5938054
Will Sturgeon <willstrgn@gmail.com>
2026-05-10 14:12:04 -0700

    Automatic build succeeded

--- flake.nix
+++ flake.nix
@@ -141,7 +141,7 @@
           unfree-regex
           username
           ;
-        default-font = "Uncut Sans Variable"; # "Instrument Sans 90"; # "Taurus Grotesk Trial"; # "Bricolage Grotesque"; # "Inter"; # "IBM Plex Sans";
+        default-font = "Taurus Grotesk Trial"; # "Bricolage Grotesque"; # "Instrument Sans 90"; # "Inter"; # "IBM Plex Sans";
         home = "/home/${username}";
         ollama-host = "127.0.0.1";
         ollama-port = 11434;
