90f4459591a4b3560c8b88c099ad1f31543d37da
Will Sturgeon <willstrgn@gmail.com>
2026-08-05 10:14:46 -0700

    Automatic build succeeded

--- doom/init.el
+++ doom/init.el
@@ -82,7 +82,7 @@
        ;;shell             ; simple shell REPL for Emacs
        ;;term              ; basic terminal emulator for Emacs
        ;;vterm             ; almost the best terminal emulation in Emacs
-       ;;ghostel           ; the best terminal emulation in Emacs
+       ghostel           ; the best terminal emulation in Emacs
 
        :checkers
        syntax              ; tasing you for every semicolon you forget
